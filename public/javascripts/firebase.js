1413e0aeaf8c68adaed3c7a163e047c461d41101
injaJoo <milk7821@inha.edu>
2023-10-07 18:40:34 +0900

    week6

---
+++ public/javascripts/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyBvBhPGPtil6g9rxdaMvCVkzroOnBq0B-0",
+authDomain: "fir-84106.firebaseapp.com",
+projectId: "fir-84106",
+storageBucket: "fir-84106.appspot.com",
+messagingSenderId: "747641690455",
+appId: "1:747641690455:web:b046e8490c3ebecff40612"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
