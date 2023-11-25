f03a962df162e962e6d0f698f05fcdac98fa6cfb
inhaJoo <milk7821@inha.edu>
2023-11-25 18:10:12 +0900

    week13

--- routes/posts.js
+++ routes/posts.js
@@ -2,18 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* page :: posting */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: '게시판', pageName:'posts/list.ejs'});
 });
 
 /* page :: posting */
-router.get('/insert', function(req, res, next) {
+router.get('/insert', function(req, res) {
   res.render('index', { title: '글쓰기', pageName:'posts/insert.ejs'});
 });
 
-router.get('/read', function(req, res, next) {
+router.get('/read', function(req, res) {
   const id=req.query.id;
-  res.render('index', { title: '게시글정보', pageName:'posts/read.ejs', id});
+  res.render('index', { title: '게시글 정보', pageName:'posts/read.ejs', id:id});
+});
+
+router.get('/update', function(req, res ) {
+  const id=req.query.id;
+  res.render('index', { title: '게시글 수정', pageName:'posts/update.ejs', id:id});
 });
 
 module.exports = router;
